e603320f9cdf432b63db10df3e73cbfa357e1dbf
Doomzeon <33652380+Doomzeon@users.noreply.github.com>
2022-01-24 21:11:36 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 load_dotenv()
 
-app = Flask(__name__)
+application = Flask(__name__)
 cors = CORS(app)
 
 logging.basicConfig(level=logging.INFO)
@@ -16,4 +16,4 @@ logging.basicConfig(level=logging.INFO)
 Router(app)
 
 if __name__ == "__main__":
-    app.run(port=80, debug=True)
+    application.run(debug=True)
